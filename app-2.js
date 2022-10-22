d2b94ca7c0220000654a022065e1358aba30fc27
Axel <lucasaxelqg15@gmail.com>
2022-10-22 06:58:08 -0300

    4th commit

--- app-2.js
+++ app-2.js
@@ -1,10 +1,8 @@
-const SECTION = document.getElementById("adaptative");
 const DIV_SCR = document.getElementById("screen");
-const PERSON = document.getElementById("person");
-const CURSOR = document.getElementById("cursor");
 const BOX = document.getElementById('cuadrado');
 const BOX2 = document.getElementById('cuadrado2');
 const BOX3 = document.getElementById('cuadrado3');
+const P = document.getElementById('p')
 
 function mover(){
     BOX.classList.add('moviendo')
@@ -12,24 +10,48 @@ function mover(){
     BOX3.classList.add('moviendo3')
 }
 mover()
+
+let score = 0
+sumar = (num) => score+=num
+
 BOX.addEventListener("click", function(){
     BOX.style.backgroundImage="url(./assets/bomb.gif)"
+    sumar(10)
     setTimeout(()=>{(
+    BOX.style.backgroundImage="none"
+)}, 900);
+setTimeout(()=>{(
     BOX.style.backgroundImage="url(./assets/kamikaze.gif)"
-)}, 1000);
+)}, 1500);
 });
 
 BOX2.addEventListener("click", function(){
     BOX2.style.backgroundImage="url(./assets/bomb2.gif)"
+    sumar(25)
     setTimeout(()=>{(
+    BOX2.style.backgroundImage="none"
+)}, 900);
+setTimeout(()=>{(
     BOX2.style.backgroundImage="url(./assets/zombi.gif)"
-)}, 2700);
+)}, 2000);
 });
 
 BOX3.addEventListener("click", function(){
     BOX3.style.backgroundImage="url(./assets/bomb3.gif)"
+    sumar(50)
     setTimeout(()=>{(
+    BOX3.style.backgroundImage="none"
+)}, 900);
+setTimeout(()=>{(
     BOX3.style.backgroundImage="url(./assets/momia.gif)"
-)}, 2300);
+)}, 2000);
 });
+
+setInterval(()=>{
+    let contador = score
+    P.innerHTML='SCORE: ' + contador
+},50)
+
+
+
 
