5fc63956ae8d19ed7ec47c0c622413c6bc1d573c
Axel <lucasaxelqg15@gmail.com>
2022-10-27 04:25:38 -0300

    levels & commits

--- app-2.js
+++ app-2.js
@@ -2,7 +2,12 @@ const DIV_SCR = document.getElementById("screen");
 const BOX = document.getElementById('cuadrado');
 const BOX2 = document.getElementById('cuadrado2');
 const BOX3 = document.getElementById('cuadrado3');
-const P = document.getElementById('p')
+const P = document.getElementById('p');
+const P2 = document.getElementById('p2')
+
+let greeting = 'Bienvenido, toque a los enemigos para explotarlos ' 
+
+P2.innerHTML=greeting
 
 function mover(){
     BOX.classList.add('moviendo')
@@ -11,14 +16,30 @@ function mover(){
 }
 mover()
 
+const changeEL = ()=>{
+    if(score>1)P2.innerHTML='Eso! Justo asi!'
+    if(score>400)P2.innerHTML='Sigue asi!!';
+    if(score>800)P2.innerHTML='800 puntos!!';
+    if(score>1000)P2.innerHTML='Gracias por jugar!';
+    if(score>1100)P2.innerHTML='Estas a punto de avanzar';
+    if(score>1200){
+        P2.innerHTML='Level 2'
+        DIV_SCR.style.backgroundImage='url(./assets/green-back.gif)'};
+    if(score>1500)P2.innerHTML='Estas a punto de avanzar';
+    if(score>2000)P2.innerHTML='Ya casi';
+    if(score>2400){
+        P2.innerHTML='Level 3'
+        DIV_SCR.style.backgroundImage='url(./assets/blue-back.gif)'};
+}
+
 let score = 0
 sumar = (num) => score+=num
-
 
 
 BOX.addEventListener("click", function(){
     BOX.style.backgroundImage="url(./assets/bomb.gif)"
     sumar(10)
+    changeEL()
     setTimeout(()=>{(
     BOX.style.zIndex='-1'
 )}, 900);
@@ -29,9 +50,9 @@ setTimeout(()=>{(
 });
 
 BOX2.addEventListener("click", function(){
-
     BOX2.style.backgroundImage="url(./assets/bomb2.gif)"
     sumar(25)
+    changeEL()
     setTimeout(()=>{(
     BOX2.style.zIndex='-1'
 )}, 900);
@@ -44,6 +65,7 @@ setTimeout(()=>{(
 BOX3.addEventListener("click", function(){
     BOX3.style.backgroundImage="url(./assets/bomb3.gif)"
     sumar(50)
+    changeEL()
 setTimeout(()=>{(
     BOX3.style.zIndex='-1'
 )}, 900);
@@ -58,4 +80,6 @@ setInterval(()=>{
     let contador = score
     P.innerHTML='SCORE: ' + contador
 },500)
+
+
 
